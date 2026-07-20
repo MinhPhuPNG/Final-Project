ef724c2b5c5ad96bc3b54c33f127f0babc23b923
MinhPhuPNG <phuminh.png@gmail.com>
2026-07-20 00:09:10 -0400

    Update ResourceCounter.cs

--- Assets/Scripts/ResourceCounter.cs
+++ Assets/Scripts/ResourceCounter.cs
@@ -1,64 +1,18 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using TMPro;
-
 public class ResourceCounter : MonoBehaviour
 {
-    public static ResourceCounter Instance { get; private set; }
-
-    [Header("UI Text References")]
     public TextMeshProUGUI mushroomText;
     public TextMeshProUGUI purpleFlowerText;
     public TextMeshProUGUI treeShroomText;
-
     private int mushrooms;
     private int purpleFlowers;
     private int treeShrooms;
     private bool hasTutorialPotion = false;
     private bool hasSpellBook = false;
-
-    private void Awake()
+    void Start()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else if (Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-    }
-
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-
-        RebindUI();
         UpdateUI();
-    }
-
-    private void RebindUI()
-    {
-        GameObject mObj = GameObject.Find("MushroomText");
-        GameObject pObj = GameObject.Find("PurpleFlowerText");
-        GameObject tObj = GameObject.Find("TreeShroomText");
-
-        if (mObj != null) mushroomText = mObj.GetComponent<TextMeshProUGUI>();
-        if (pObj != null) purpleFlowerText = pObj.GetComponent<TextMeshProUGUI>();
-        if (tObj != null) treeShroomText = tObj.GetComponent<TextMeshProUGUI>();
     }
 
     public int GetMushroomCount() => mushrooms;
@@ -75,12 +29,11 @@ public class ResourceCounter : MonoBehaviour
         
         UpdateUI();
     }
-
     private void UpdateUI()
     {
-        if (mushroomText != null) mushroomText.text = "Mushrooms: " + mushrooms;
-        if (purpleFlowerText != null) purpleFlowerText.text = "Flowers: " + purpleFlowers;
-        if (treeShroomText != null) treeShroomText.text = "Shrooms: " + treeShrooms;
+        mushroomText.text = "Mushrooms: " + mushrooms;
+        purpleFlowerText.text = "Flowers: " + purpleFlowers;
+        treeShroomText.text = "Shrooms: " + treeShrooms;
     }
 
     public void AddResource(string resourceName, int amount)
